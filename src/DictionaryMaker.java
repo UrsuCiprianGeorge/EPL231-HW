06235b684504450c461116c583d8c2bbb09215b2
marios-an <mandre08@ucy.ac.cy>
2025-11-26 18:52:09 +0200

    start DictionaryMaker

--- src/DictionaryMaker.java
+++ src/DictionaryMaker.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class DictionaryMaker {
 
@@ -7,6 +12,31 @@ public class DictionaryMaker {
         if (!wordsFile.exists()) {
             System.out.println("File " + wordsFile.getAbsolutePath() + " does not exist.");
             System.exit(1);
+        }
+
+
+        int[] wordlengths = new int[34];
+        int[][] letter;
+
+
+
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(wordsFile.toURI()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\t");
+                String col1 = parts[0];
+                long col2 = Long.parseLong(parts[1]);
+
+
+
+
+
+
+
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
 
